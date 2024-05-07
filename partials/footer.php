95cd4c511e09af26d77463c4f7bc1d7b625a9edd
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-05-07 17:39:27 +0200

    ADD: added header and footer including them in the index.php file

---
+++ partials/footer.php
@@ -0,0 +1,5 @@
+<footer class="container text-white  px-0 pt-4">
+    <div class="row align-items-baseline"> 
+        <div class="col-auto">Made by Vicio :D</div>
+    </div>
+</footer>
---
+++ partials/header.php
@@ -0,0 +1,6 @@
+<header class="container text-white  px-0 pt-4">
+    <div class="row align-items-baseline">
+        <div class="col-auto h1 mb-0 me-auto">BOOLEAN NEWSLETTER</div>    
+        <div class="col-auto">Iscriviti gratuitamente</div>
+    </div>
+</header>
