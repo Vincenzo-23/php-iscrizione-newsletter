8d05aa02fd5db236754bcf356c26959b95b101bd
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-05-07 17:20:41 +0200

    ADD: added some other styles with Bootstrap

--- index.php
+++ index.php
@@ -14,20 +14,20 @@ require_once __DIR__ . "/utilities.php";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body class="bg-primary">
-    <div class="container p-4 mt-5 border border-bottom-0 border-info border-4">
+    <div class="container p-4 mt-5 border border-bottom-0 border-info border-4 text-center">
         <form action="index.php" method="POST">
             <p>
                 <label for="email" class="fw-bold me-3 text-white">Iscriviti alla nostra newsletter</label>
-                <input type="email" name="email" placeholder="Inserisci la tua email">
+                <input type="email" name="email" placeholder="Inserisci la tua email" class="w-25">
             </p>
             <button class="btn btn-info rounded px-4 mt-4 fw-bold text-white">Invia</button>
         </form>
     </div>
-    <div class="container p-4 border border-top-0 border-info border-4">
+    <div class="container text-center fw-bold p-4 border border-top-0 border-info border-4">
         <?php
         if($email === ""){ 
         ?>
-            <div class="alert alert-primary" role="alert">
+            <div class=" alert alert-primary" role="alert">
                 Inserisci la tua email nell'apposito campo!
             </div>
         <?php 
