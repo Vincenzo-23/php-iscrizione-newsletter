ea72548d6bf916ce3a12620c1f997f2c3453b57b
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-05-07 14:49:55 +0200

    Created index.php file and added the form inside it

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Newsletter</title>
+</head>
+<body>
+    <form action="index.php" method="POST">
+        <p>
+            <label for="mail">Iscriviti alla nostra newsletter</label>
+            <input type="email" placeholder="Inserisci la tua email">
+        </p>
+        <button>Invia</button>
+    </form>
+</body>
+</html>
